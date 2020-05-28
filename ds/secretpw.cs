55d796b50e2caff133e1e22cf05963529375a59a
Banti <bantlushaj@gmail.com>
2020-05-28 02:29:52 +0200

    hide-password -> done

---
+++ ds/secretpw.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace ds
+{
+    class secretpw
+    {
+        public static void secret_password()
+        {
+            Console.Write("You password please: ");
+            StringBuilder passwordBuilder = new StringBuilder();
+            bool continueReading = true;
+            char newLineChar = '\r';
+            while (continueReading)
+            {
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey(true);
+                char passwordChar = consoleKeyInfo.KeyChar;
+
+                if (passwordChar == newLineChar)
+                {
+                    continueReading = false;
+                }
+                else
+                {
+                    passwordBuilder.Append(passwordChar.ToString());
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Your password in plain text is {0}", passwordBuilder.ToString());
+        }
+    }
+}
