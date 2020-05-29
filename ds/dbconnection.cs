da8be34f34f253e1e3b6ddce804494bf0e37e166
Banti <bantlushaj@gmail.com>
2020-05-30 01:34:00 +0200

    users info(statements) -> sql ✅

--- ds/dbconnection.cs
+++ ds/dbconnection.cs
@@ -9,9 +9,29 @@ namespace ds
     {
         public static void Conn()
         {
-            string cs = @"server=localhost;userid=root;password=;database=ds";
-            var con = new MySqlConnection(cs);
-            con.Open();
+            string connectionstring = @"server=localhost;userid=root;password=7834;database=ds";
+            var con = new MySqlConnection(connectionstring);
+
+            string banti = "Shaban";
+            string mysqlStatement = "insert into users(uname,salt, password) values('" + banti + "' ,  '" + Random() + "','" + Hash(banti) + "');";
+            MySqlCommand commands = new MySqlCommand(mysqlStatement, con);
+            try
+            {
+                con.Open();
+                int fillRows = commands.ExecuteNonQuery();
+                if (fillRows == 1)
+                {
+                   Console.WriteLine("Success!");
+                }
+                else
+                {
+                    Console.WriteLine("Error!");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ka ndodhur nje gabim" + e);
+            }
         }
 
         static string Random()
@@ -20,6 +40,7 @@ namespace ds
             string randString = Rand.ToString();
             return randString;
         }
+
         static string Hash(string plain)
         {
             SHA256CryptoServiceProvider objHash = new SHA256CryptoServiceProvider();
@@ -33,7 +54,5 @@ namespace ds
             string hash = Convert.ToBase64String(computeHash);
             return hash;
         }
-        
-        // tbd > mysql commands to save in db
     }
 }
