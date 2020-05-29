eb97f65ea8c8723a8af324eac8f639ea6cd912cf
Banti <bantlushaj@gmail.com>
2020-05-30 01:04:17 +0200

    random salt generator + plaintext -> hash

--- ds/dbconnection.cs
+++ ds/dbconnection.cs
@@ -1,7 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
 using System.Text;
+using System.Security.Cryptography;
 
 namespace ds
 {
@@ -14,7 +14,26 @@ namespace ds
             con.Open();
         }
 
-        // user -> emri i user
-        // password -> salt + hash
+        static string Random()
+        {
+            int Rand = new Random().Next(1000000, 10000000);
+            string randString = Rand.ToString();
+            return randString;
+        }
+        static string Hash(string plain)
+        {
+            SHA256CryptoServiceProvider objHash = new SHA256CryptoServiceProvider();
+
+            string randString = Random();
+
+            string salt_password = plain + randString;
+            byte[] hashGet = Encoding.UTF8.GetBytes(salt_password);
+
+            byte[] computeHash = objHash.ComputeHash(hashGet);
+            string hash = Convert.ToBase64String(computeHash);
+            return hash;
+        }
+        
+        // tbd > mysql commands to save in db
     }
 }
